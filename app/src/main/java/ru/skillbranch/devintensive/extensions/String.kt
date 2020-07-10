27fc99a6a32290ca349d1caee22d440313ae8f32
xanaks <nazar2539@gmail.com>
2020-07-10 03:58:45 +0300

    HELP_ME_PLS_FUCKING_PROBLEM_GRADLE

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,18 @@
 package ru.skillbranch.devintensive.extensions
 
+
+fun String.stripHtml() : String{
+    var resultString: String
+    resultString = this.substringBefore("<") + this.substringAfter(">")
+    if(resultString.contains("<"))
+        resultString = resultString.stripHtml()
+    return resultString.replace(Regex("\\s+")," ").replace(Regex("[& < > ' \"\"]")," ")
+}
+
+fun String.truncate(lenght: Int = 16) : String{
+    val filler: String = "..."
+    return this.substring(0, lenght).trimEnd { c ->  c.equals(' ') } + filler
+
+}
+
+
